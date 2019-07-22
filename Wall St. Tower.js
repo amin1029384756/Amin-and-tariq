56ef75d1eb99dc017503d89afaf64b1d2b3a6fce
tariq7A <52589370+tariq7A@users.noreply.github.com>
2019-07-21 17:23:35 -0700

    Update Wall St. Tower.js

--- Wall St. Tower.js
+++ Wall St. Tower.js
@@ -12,3 +12,8 @@ textSize(25);
 text(gh + "$",50, 385);
 if(keyIsPressed && keyCode === UP){}
 if(keyIsPressed && keyCode === DOWN){}
+fill(9, 0, 255);
+rect(130, 360, 30, 30);
+fill(255, 255, 255);
+textSize(30);
+text("f", 140, 390);
