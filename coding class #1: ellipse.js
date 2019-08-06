040debc245b5a5231e11d2705506ed3e1b0651be
tariq7A <52589370+tariq7A@users.noreply.github.com>
2019-08-05 18:50:35 -0700

    Create coding class #1: ellipse.js
    for our youtube channel...

---
+++ coding class #1: ellipse.js
@@ -0,0 +1,20 @@
+
+//face
+ellipse(200, 200, 300, 300);
+//mouth
+ellipse(200, 250, 150, 50);
+//eyes
+ellipse(150, 150, 30, 30);
+ellipse(250, 150, 30, 30);
+
+
+//then make it smaller...
+
+//face
+ellipse(200, 200, 200, 200);
+//mouth
+ellipse(200, 250, 100, 50);
+//eyes
+ellipse(160, 150, 20, 20);
+ellipse(240, 150, 20, 20);
+
